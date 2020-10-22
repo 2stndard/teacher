d4e18112f1308ce24aff36b657d387e75dd9cce9
이기준 <2stndard@gmail.com>
2020-10-22 17:56:36 +0900

    1022-1

--- map.R
+++ map.R
@@ -24,23 +24,63 @@ if (!require(numbers)) {
   library(numbers)
 }
 
-if (!require(Math)) {
-  install.packages('Math', repos = 'https://cloud.r-project.org/')
-  library(Math)
-}
 library(tidyverse)
 
-korea_map_shp = readShapePoly("D:/R/git/teacher/map/CTPRVN.shp")
-korea_map = fortify(korea_map_shp)
+province.name <- c('11', '26', '27', '28', '29', '30', '31', '36', '41', '42', '43', '44', '45', '46', '47', '48', '50')
 
-korea_map %>% 
-  filter(group %in% c('0.1', '1.1','2.1','3.1','4.1','5.1','6.1','7.1','8.1','9.1','10.1','11.1','12.1','13.1','14.1','15.1','16.1')) %>%  
-  plot_ly(x = ~long, y = ~lat, split = ~group, color = I('white'), showlegend = F) %>%  
-  add_polygons(line = list(width = 0.4, color = toRGB('black'))) %>%
+shapes <- data.frame()
+
+read.shp <- function(x) {
+  return(fortify(readShapePoly(paste0("D:/R/data/map/", x, "000/TL_SCCO_CTPRVN.shp"))) %>%  mutate(region = x))
+}
+
+shapes <- map(province.name, read.shp)
+
+shapes.t <- data.frame()
+
+for(i in 1:17) {
+  shapes.t <- rbind(shapes.t, shapes[[i]])
+}
+
+shapes.t %>%  
+  filter(group %in% c('0.1'))  %>% #filter(region == '46') %>%
+  plot_ly(x = ~long, y = ~lat, split = ~paste0(region, group), color = I('white'), showlegend = F) %>%  
+  add_polygons(line = list(width = 1, color = toRGB('red'))) %>%
   layout(xaxis = list(scaleanchor = 'y', showgrid = F, showticklabels = F, title = NA),
          yaxis = list(showgrid = F, showticklabels = F, title = NA)
-         )
-         
-         
-korea_map %>% mutate(group1 = as.numeric(as.character(korea_map$group)) - as.numeric(id)) %>%  
-  filter(group %in% c('1.1')) %>% head
+  )
+
+shapes.t %>%  
+  filter(group %in% c('0.1')) %>% filter(region == '29')
+
+
+dir <- ('D:/R/data/map/location/')
+
+file.list <- list.files(dir, pattern = 'entrc_')
+
+data <- data.frame()
+
+for(file in file.list) {
+  print(file)
+  temp <- read.delim(paste0(dir, file), sep = '|', header = F)
+  data <- rbind(data, temp)
+}
+
+d <- data %>% filter(is.na(V17) == F, is.na(V18) == F, V14 %in% '교육및복지시설')
+
+d <- data %>% filter(is.na(V17) == F, is.na(V18) == F, grepl('대학교', V12), V14 %in% '교육및복지시설')
+
+dim(d)
+dim(data)
+
+shapes.t %>%  
+  filter(group %in% c('0.1'))  %>% #filter(region == '46') %>%
+  plot_ly(x = ~long, y = ~lat, color = I('white'), showlegend = F) %>%  
+  add_polygons(split = ~paste0(region, group), line = list(width = 1, color = toRGB('red'))) %>%
+  layout(xaxis = list(scaleanchor = 'y', showgrid = F, showticklabels = F, title = NA),
+         yaxis = list(showgrid = F, showticklabels = F, title = NA)
+  ) %>%
+  add_data(data = d) %>%
+  add_markers(x = ~V17, y = ~V18, marker = list(size = 3, color = toRGB('blue'), opacity = 1), 
+              hoverinfo = 'text', 
+              text = ~V12)
